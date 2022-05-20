5b46d0df4a1098ba4a077b85ea5ecbb9b54f1201
Mihir Aswal <montyaswal0@gmail.com>
2022-05-20 17:57:40 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor(){
     super();
     this.state={
-      monsters:[]
+      monsters:[],
+      search:''
      
     };
   
@@ -22,10 +23,30 @@ class App extends Component {
     ))
     // .then((users)=>console.log(users));
   }
+  onsearchChange=(event)=>{ 
+    const searchString =event.target.value.toLowerCase();//it will contain the string we type in box
+
+      this.setState(()=>{
+       return {search:searchString}; //to set the state of new find elmnts
+     }
+     );
+    }
   render(){
+    const{monsters,search}=this.state;
+    const{onsearchChange}=this;
+    
+    const filteredMonster=monsters.filter((monster)=>{    //this.state.monsters.filter tha replace krdiya
+      return monster.name.toLowerCase().includes(search);   //this.state.search        //an array of filter monster based on the search
+      });
     return (
       <div className="App">
-        {this.state.monsters.map((monster)=>{
+        <input className='search-box' type='search' placeholder='Search Monsters' onChange={onsearchChange //this.onsearchange
+
+        }
+         />
+
+        {
+        filteredMonster.map((monster)=>{
           // return <h1 key={monster.id}>{monster.name}</h1>;
           return <div key={monster.id}>
             <h1>{monster.name}</h1>
